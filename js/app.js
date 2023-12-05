0288d3765442df63613e199f27d3273421d2d5d9
bscantor3 <bscantor3@gmail.com>
2023-12-05 01:51:46 -0500

    new version

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-particlesJS.load("particles-js", "assets/particles.json", function () {
+particlesJS.load("particles-js", "./assets/particles.json", function () {
   console.log("callback - particles.js config loaded");
 });
 
